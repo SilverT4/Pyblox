a3b1ebab0ac3f1c4b0e7514d8ce6802ccce36c90
Silver <silvertn4@gmail.com>
2025-02-14 15:51:52 -0500

    add main module

---
+++ __main__.py
@@ -0,0 +1,7 @@
+__doc__ = """Main module for Pyblox, an attempt to recreate what Bloxstrap does but in Python.
+
+Credits:
+
+@SilverT4 - Project owner
+
+The Bloxstrap developers and contributors - Bloxstrap"""
